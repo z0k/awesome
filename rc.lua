b7d3b62394341776e8fcaf4879168a28884f8371
Zarak <zarak.mahmud@gmail.com>
2015-09-23 23:16:22 +0500

    Add cpu usage widget.

--- rc.lua
+++ rc.lua
@@ -42,7 +42,16 @@ do
 end
 -- }}}
 
+-- Initialize widget
+cpuwidget = awful.widget.graph()
+-- Graph properties
+cpuwidget:set_width(50)
+cpuwidget:set_background_color("#494B4F")
+cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
+                    {1, "#AECF96" }}})
 
+-- Register widget
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
 -- Initialize widget
 memwidget = awful.widget.progressbar()
 -- Progressbar properties
@@ -225,6 +234,7 @@ for s = 1, screen.count() do
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(volume_widget)
+    right_layout:add(cpuwidget)
     right_layout:add(memwidget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
