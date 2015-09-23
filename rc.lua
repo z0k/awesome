b9dbf46a01827b956200a6c3e3d61c917f029f74
Zarak <zarak.mahmud@gmail.com>
2015-09-23 23:20:08 +0500

    Change position of cpu usage widget.

--- rc.lua
+++ rc.lua
@@ -233,9 +233,9 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(memwidget)
     right_layout:add(volume_widget)
     right_layout:add(cpuwidget)
-    right_layout:add(memwidget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
